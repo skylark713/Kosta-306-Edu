e39b67e74394666812f1139f395722d382d6e906
Yejin Lee <wthb22923@gmail.com>
2026-03-25 17:29:43 +0900

    2026.03.25 파일 업로드

---
+++ VS_Work/step03_JavaScript/js/basic.js
@@ -0,0 +1,15 @@
+// 여기는 자바스크립트 영역입니다.
+
+/*여러줄 또는 부분 주석입니다.*/
+
+//콘솔에 출력
+console.log("어디에 출력되나요?");
+
+//브라우저에 출력
+document.write("<h1>난 어디에 나오니?</h1>");
+
+//함수작성
+function test(th) {
+  th.style.backgroundColor = "red";
+  th.style.color = "white";
+}
---
+++ VS_Work/step03_JavaScript/js/test.js
@@ -0,0 +1,14 @@
+//콘솔 출력
+console.log("1.콘솔창에 출력되어요^^");
+
+// h1태그를 클릭하면 css를 적용하고 싶다. - 함수를 작성
+function text(th) {
+  // th는 this가 전달됨
+  // alert("h1 태그 클릭하셨네요?");
+  th.style.backgroundColor = "red";
+}
+
+for (i = 0; i < 10000; i++) {}
+
+//alert("끝~~");
+console.log("test.js 끝");
